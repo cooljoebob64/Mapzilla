4273536565c443161d44a75bb6cb2da569d4ad1b
Joshua Luppes <cooljoebob64@gmail.com>
2021-03-08 08:33:15 -0600

    Create initial project files from Spring Initializr

---
+++ src/main/java/com/jlu/mapzilla/MapzillaApplication.java
@@ -0,0 +1,13 @@
+package com.jlu.mapzilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapzillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapzillaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jlu/mapzilla/MapzillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlu.mapzilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapzillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
