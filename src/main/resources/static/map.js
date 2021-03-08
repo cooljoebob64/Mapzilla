19f9b1dc909629d7d9bdc9cd84d6c0c42dfb49b9
Joshua Luppes <cooljoebob64@gmail.com>
2021-03-08 10:02:47 -0600

    Add custom marker image

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -6,8 +6,24 @@ function initMap() {
         zoom: 10,
         scrollwheel: false
     });
+    var zilla = "/zilla.png"
     var marker = new google.maps.Marker({
-    position: coords,
-    map: map
+        position: coords,
+        map: map,
+        icon: {
+            url:zilla,
+            scaledSize: new google.maps.Size(50, 50)
+            },
+        animation: google.maps.Animation.BOUNCE
     });
+
+    var contentString = '<h2>Columbus, OH</h2> <p>Where your dreams come true.</p>';
+
+     var infowindow = new google.maps.InfoWindow({
+       content: contentString
+     });
+
+     google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+     });
 }
