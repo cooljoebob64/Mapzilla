1416fe18981d9c7dd28b53b214e44a0ee530917e
Joshua Luppes <cooljoebob64@gmail.com>
2021-03-08 16:54:50 -0600

    Update formatting for HTML, CSS, and JS files

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -1,27 +1,28 @@
 let map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: coords,
-        zoom: 12,
-        scrollwheel: false
-    });
-    let zilla = "/zilla.png"
-    let marker = new google.maps.Marker({
-        position: coords,
-        map: map,
-        icon: {
-            url:zilla,
-            scaledSize: new google.maps.Size(50, 50)
-            },
-        animation: google.maps.Animation.BOUNCE
-    });
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: coords,
+    zoom: 12,
+    scrollwheel: false,
+  });
+  let zilla = "/zilla.png";
+  let marker = new google.maps.Marker({
+    position: coords,
+    map: map,
+    icon: {
+      url: zilla,
+      scaledSize: new google.maps.Size(50, 50),
+    },
+    animation: google.maps.Animation.BOUNCE,
+  });
 
-let contentString = '<h2>' + city + ', ' + state + ', where your dreams come true!</h2>';
-     let infowindow = new google.maps.InfoWindow({
-       content: contentString
-     });
+  let contentString =
+    "<h2>" + city + ", " + state + ", where your dreams come true!</h2>";
+  let infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
 
-     google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map,marker);
-     });
+  google.maps.event.addListener(marker, "click", function () {
+    infowindow.open(map, marker);
+  });
 }
